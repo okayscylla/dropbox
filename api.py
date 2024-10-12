415fc1cc7bb260bcf0cc9276d91e8c8eb42bc997
Annabelle <annabelleisundercover@gmail.com>
2024-10-12 18:04:56 -0400

    cleaning up formatting

--- api.py
+++ api.py
@@ -2,18 +2,18 @@ import dropbox
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logging.getLogger('requests').setLevel(logging.DEBUG)
-logging.getLogger('dropbox').disabled = True
+logging.getLogger("requests").setLevel(logging.DEBUG)
+logging.getLogger("dropbox").disabled = True
 
 
 with open("secrets.txt", "r") as f:
-    ACCESS_TOKEN=f.read()
+    ACCESS_TOKEN = f.read()
 
 
 class DropboxClient:
-    def __init__(self,access_token):
+    def __init__(self, access_token):
         self.access_token = access_token
-        
+
         self.initialised = False
         self.connected = False
         self.authenticated = False
@@ -24,20 +24,24 @@ class DropboxClient:
             self.initialised = True
             self.connected = True
         except dropbox.exceptions.AuthError:
-            logging.error("Error connecting to Dropbox with access token: " + self.access_token)
+            logging.error(
+                "Error connecting to Dropbox with access token: " + self.access_token
+            )
             self.initialised = True
         except:
             raise
-        
+
         try:
             self.account = self.instance.users_get_current_account()
-            logging.info(f"Connected to Dropbox with account {self.account.name.display_name}")
+            logging.info(
+                f"Connected to Dropbox with account {self.account.name.display_name}"
+            )
             self.authenticated = True
         except dropbox.exceptions.AuthError:
             logging.error("Failed to get current account")
         except:
             raise
-        
+
     def explore(self, path):
         try:
             return self.instance.files_list_folder(path).entries
@@ -46,7 +50,7 @@ class DropboxClient:
                 logging.error(f"Folder {path} not found")
             else:
                 raise
-    
+
     def download(self, file, destination):
         try:
             self.instance.files_download_to_file(file, destination)
@@ -54,7 +58,7 @@ class DropboxClient:
             if e.error.is_path() and e.error.get_path().is_not_found():
                 print(f"File {file} not found")
             else:
-                raise 
+                raise
 
 
 if __name__ == "__main__":
--- widgets.py
+++ widgets.py
@@ -5,9 +5,11 @@ import os
 
 
 class PopupWindow(customtkinter.CTk):
-    def __init__(self, title, resolution=(430, 180), scale=1.0, *args, **kwargs): # TODO implement icons
+    def __init__(
+        self, title, resolution=(430, 180), scale=1.0, *args, **kwargs
+    ):  # TODO implement icons
         super().__init__(*args, **kwargs)
-        
+
         self.title(title)
         self.scale = scale
         self.geometry(f"{int(resolution[0]*scale)}x{int(resolution[1]*scale)}")
@@ -22,54 +24,57 @@ class BrowseFrame(customtkinter.CTkScrollableFrame):
         self.handle = handle
         self.scale = scale
         self.directory = None
-        
+
         if self.startup_test() is not True:
             raise Exception("There was an error with the handle")
-        
+
         self.seek(str(), absolute=True)
         self.refresh()
-    
+
     def startup_test(self):
         if not self.handle.initialised:
             logging.error("Handle not initialised")
-            return 0 #Handle not initialised
+            return 0  # Handle not initialised
         elif not self.handle.connected:
             logging.error("Handle not connected")
-            return -1 #Handle not connected
+            return -1  # Handle not connected
         elif not self.handle.authenticated:
             logging.error("Handle not authenticated")
-            return -2 #Handle not authenticated
-        
-        logging.info(f"Startup test passed: initialised: {self.handle.initialised}, connected: {self.handle.connected}, authenticated: {self.handle.authenticated}")
+            return -2  # Handle not authenticated
+
+        logging.info(
+            f"Startup test passed: initialised: {self.handle.initialised}, connected: {self.handle.connected}, authenticated: {self.handle.authenticated}"
+        )
         return True
 
     def seek(self, path, absolute=False):
         logging.info(f"Leaving directory {self.directory.__repr__()}")
-        
+
         if type(path) is not str:
             logging.error("Error while seeking: path must be a string")
             return False
-        
+
         if absolute:
             logging.info(f"Seeking to absolute path: {path.__repr__()}")
             self.directory = path
         else:
             logging.info(f"Seeking to relative path: {path.__repr__()}")
             self.directory = os.path.join(self.directory, path)
-        
+
         return True
 
     def refresh(self):
         pass
+
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
     root.title("Test")
     root.geometry("1200x740")
     root.resizable(False, False)
-    
+
     handle = dbx.DropboxClient(dbx.ACCESS_TOKEN)
-    
+
     frame = BrowseFrame(root, handle)
     frame.pack(expand=True, fill="both")
 
