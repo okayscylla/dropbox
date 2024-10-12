16073a190519f63ab31d6305971df87834b4fb56
Annabelle <annabelleisundercover@gmail.com>
2024-10-12 18:29:02 -0400

    .

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ def access_secret():
     with open("secrets.txt", "r") as f:
         return f.read()
 
-def bad_secret():
+def bad_secret(): # TODO generate this
     return "sl.B98fvKZy8KTwGqiGLmGQUNXT6HcNFm2U4_VpMTvYwfl5lFpaCxli8zA9eWV9ugNnnwpm2Pj5mm5TElIPqiRGDVZZSSlEye5NG2tuMaWBTQSjQMWRsMzYmcG-BOJdYesnnMBJ33f6zOFS"
 
 
